9edfce8ef4b717b5d844d154c384ab13a7ce2de7
sunil <sunilyadavkpy@gmail.com>
2024-01-15 19:59:11 +0530

    created file

---
+++ for loop.R
@@ -0,0 +1,17 @@
+#Problem 1- Write your name 10 times.
+#Problem 2- Write the table of 9
+#Problem 3- Print all prime numbers between 1 to 100
+
+for (i in 1:10){
+  print("Sunil")
+}
+
+for (i in 1:10){
+  print(9*i)
+}
+
+for (i in 1:10){
+  print(11*i)
+}
+
+
